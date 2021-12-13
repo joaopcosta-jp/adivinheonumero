c68880881c48f32a3d9bb82a522872d81c5b1c76
João Pedro Costa <89428776+joaopcosta-jp@users.noreply.github.com>
2021-12-13 21:55:55 +0000

    opções adicionadas

--- main.c
+++ main.c
@@ -4,13 +4,25 @@
 
 int main(void){
 
+  int op;
+
+  printf("ADIVINHE O NÚMERO\n");
+  printf("\n1 - Iniciar\n");
+  printf("2 - Sair\n");
+  printf("\nSelecione uma opção: ");
+  scanf("%d", &op);
+
+while(op !=2){
+
+  if(op == 1){
+
   srand(time(NULL));
   int n = rand() % 100;
   int n1;
   
-  printf("Adivinhe o número que estou pensando de 1 a 100\n");
+  printf("\nAdivinhe o número que estou pensando de 1 a 100\n");
   
-  printf("Digite um número inteiro: ");
+  printf("\nDigite um número inteiro: ");
   while (n1 != n) {
 
     scanf("%d", &n1);
@@ -22,13 +34,27 @@ int main(void){
     }
 
     else if (n1 > n) {
-    printf("VOCÊ ERROU!\n");
-    printf("Tente novamente\n \n");
-    printf("Digite um número menor: ");
+    printf("\nVOCÊ ERROU!\n");
+    printf("Tente novamente\n");
+    printf("\nDigite um número menor: ");
     }
      
     else if (n1 == n) {
-    printf("PARABÉNS VOCÊ ACERTOU!\n");
+    printf("\nPARABÉNS VOCÊ ACERTOU!\n");
+    printf("\nQuer jogar de novo?\n");
+    printf("\n1 - Jogar novamente\n");
+    printf("2 - Sair\n");
+    printf("\nSelecione uma opção: ");
+    scanf("%d", &op);
+    }
+  }
+    }
+  else if (op < 1 || op > 2) {
+    printf("\n*OPÇÃO INVALIDA*\n");
+    printf("\n1 - Iniciar\n");
+    printf("2 - Sair\n");
+    printf("\nEscolha outra opção: ");
+    scanf("%d", &op);
     }
   }
   return 0;
